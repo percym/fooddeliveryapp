95cbd25a00ceb09a26e8ce9be7719142da656d5f
unknown <mugadzap@gmail.com>
2026-01-06 13:42:34 +0000

    working splash screen

--- app/src/main/java/dev/percym/fooddeliveryapp/Activity/Splash/SplashActivity.kt
+++ app/src/main/java/dev/percym/fooddeliveryapp/Activity/Splash/SplashActivity.kt
@@ -7,13 +7,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import dev.percym.fooddeliveryapp.Activity.BaseActivity
 import dev.percym.fooddeliveryapp.MainActivity
@@ -43,9 +52,47 @@ fun SplashScreen(onGetStartedClick:()->Unit={}){
                 modifier = Modifier.constrainAs(backgroundImg){
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
-                }
+                }.fillMaxWidth()
             )
+
+            Image(
+                painter = painterResource(id= R.drawable.pizza),
+                contentDescription = null,
+                modifier = Modifier.run{
+                    constrainAs(logImg){
+                        top.linkTo(backgroundImg.top)
+                        bottom.linkTo(backgroundImg.bottom)
+                        start.linkTo(backgroundImg.start)
+                        end.linkTo(backgroundImg.end)
+                    }
+                }, contentScale = ContentScale.Fit
+            )
+        }
+            val styledText = buildAnnotatedString {
+                append("Welcome to your")
+                withStyle(style = SpanStyle(color = colorResource(R.color.orange))){
+                    append("food \n paradise")
+                }
+                append(" experience food perfection delivered")
+            }
+            Text(
+                text = styledText,
+                fontSize = 27.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.white),
+                modifier=Modifier.padding(top = 32.dp)
+                    .padding(horizontal = 16.dp)
+            )
+            Text(
+                text = stringResource(R.string.splashSubTitle),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.white),
+                modifier=Modifier.padding(16.dp)
+            )
+            GetStartedButton(onGetStartedClick, modifier = Modifier.padding(16.dp) )
+
+
         }
     }
 
-}
